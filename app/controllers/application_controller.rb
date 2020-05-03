4cca8c2bc29e5bbef96dfa97938ba3da14420fcc
Stefanos <stefanosu422@gmail.com>
2020-05-03 03:06:15 -0400

    added snakecase method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :underscore_params!
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :null_session 
+  protect_from_forgery  respond_to :json,  with: :null_session
+
+
+
+def underscore_params! 
+  params.deep_transform_keys!(&_underscore)
+end
+
+
 end
