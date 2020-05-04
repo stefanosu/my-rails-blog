82b2a1a87f38b2d5e931418df12702a50c48754a
Stefanos <stefanosu422@gmail.com>
2020-05-04 19:20:20 -0400

    added respon_to json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,13 @@ class ApplicationController < ActionController::Base
   before_action :underscore_params!
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery  respond_to :json,  with: :null_session
+  protect_from_forgery with: :null_session
+  respond_to :json
 
 
 
 def underscore_params! 
-  params.deep_transform_keys!(&_underscore)
+  params.deep_transform_keys!(&:underscore)
 end
 
 
